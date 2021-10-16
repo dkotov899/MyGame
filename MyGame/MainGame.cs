9b19a9bbf28c8e16934eb582f64fca61cec15ae5
unknown <dkotov899@gmail.com>
2021-10-16 10:28:21 +0300

    Screen

--- MyGame/MainGame.cs
+++ MyGame/MainGame.cs
@@ -6,42 +6,83 @@ namespace MyGame
 {
     internal class MainGame : Game
     {
-        private GraphicsDeviceManager _graphics;
+        // GAME STATES:
+        private enum GameState { Intro, Menu, Play }
+        private GameState _gameState = GameState.Menu;
+
+        // DISPLAY                  
+        private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //------------------
+        // C O N S T R U C T
+        //------------------
         public MainGame()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = (int)ScreenManager.Instance.Dimensions.X,
+                PreferredBackBufferHeight = (int)ScreenManager.Instance.Dimensions.Y,
+                IsFullScreen = false,
+                PreferredDepthStencilFormat = DepthFormat.Depth16,
+                GraphicsProfile = GraphicsProfile.HiDef
+            };
+
+            Window.IsBorderless = false;
+            //Window.Position = new Point(0, 0);
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
+        //--------
+        // I N I T
+        //--------
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
+        //--------
+        // L O A D
+        //--------
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            ScreenManager.Instance.LoadContent(Content);
         }
 
+        //--------
+        // U N L O A D
+        //--------
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+
+            ScreenManager.Instance.UnloadContent();
+        }
+
+        //------------
+        // U P D A T E
+        //------------
         protected override void Update(GameTime gameTime)
         {
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
+
+            ScreenManager.Instance.Update(gameTime);
         }
 
+        //--------
+        // D R A W
+        //--------
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            ScreenManager.Instance.Draw(_spriteBatch);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
