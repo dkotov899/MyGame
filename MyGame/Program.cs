cb241c819afd7dcf3363373eef6d3ec8e5fc9a40
unknown <dkotov899@gmail.com>
2021-10-16 06:52:33 +0300

    create Main game

---
+++ MyGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyGame
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
