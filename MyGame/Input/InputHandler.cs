65ad62f28ee7a2d2dd80491b0094004147d56689
unknown <dkotov899@gmail.com>
2021-10-16 16:34:36 +0300

    Mouse input

--- MyGame/Input/InputHandler.cs
+++ MyGame/Input/InputHandler.cs
@@ -3,10 +3,15 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MyGame.Input
 {
+    public enum MouseButton { Left, Middle, Right }
+
     public class InputHandler : GameComponent
     {
         private static KeyboardState _keyboardState;
         private static KeyboardState _lastKeyboardState;
+
+        private static MouseState mouseState;
+        private static MouseState lastMouseState;
 
         public static KeyboardState KeyboardState
         {
@@ -18,10 +23,21 @@ namespace MyGame.Input
             get { return _lastKeyboardState; }
         }
 
+        public static MouseState MouseState
+        {
+            get { return mouseState; }
+        }
+
+        public static MouseState LastMouseState
+        {
+            get { return lastMouseState; }
+        }
+
         public InputHandler(Game game)
         : base(game)
         {
             _keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
         }
 
         public override void Initialize()
@@ -34,12 +50,16 @@ namespace MyGame.Input
             _lastKeyboardState = _keyboardState;
             _keyboardState = Keyboard.GetState();
 
+            lastMouseState = mouseState;
+            mouseState = Mouse.GetState();
+
             base.Update(gameTime);
         }
 
         public static void Flush()
         {
             _lastKeyboardState = _keyboardState;
+            lastMouseState = mouseState;
         }
 
         public static bool KeyReleased(Keys key)
@@ -57,6 +77,144 @@ namespace MyGame.Input
         public static bool KeyDown(Keys key)
         {
             return _keyboardState.IsKeyDown(key);
+        }
+
+        public static Point MouseAsPoint
+        {
+            get { return new Point(mouseState.X, mouseState.Y); }
+        }
+
+        public static Vector2 MouseAsVector2
+        {
+            get { return new Vector2(mouseState.X, mouseState.Y); }
+        }
+
+        public static Point LastMouseAsPoint
+        {
+            get { return new Point(lastMouseState.X, lastMouseState.Y); }
+        }
+
+        public static Vector2 LastMouseAsVector2
+        {
+            get { return new Vector2(lastMouseState.X, lastMouseState.Y); }
+        }
+
+        public static bool CheckMousePress(MouseButton button)
+        {
+            bool result = false;
+
+            switch (button)
+            {
+                case MouseButton.Left:
+
+                    result = mouseState.LeftButton == ButtonState.Pressed &&
+                        lastMouseState.LeftButton == ButtonState.Released;
+
+                    break;
+
+                case MouseButton.Right:
+
+                    result = mouseState.RightButton == ButtonState.Pressed &&
+                        lastMouseState.RightButton == ButtonState.Released;
+
+                    break;
+
+                case MouseButton.Middle:
+
+                    result = mouseState.MiddleButton == ButtonState.Pressed &&
+                        lastMouseState.MiddleButton == ButtonState.Released;
+
+                    break;
+            }
+
+            return result;
+        }
+
+        public static bool CheckMouseReleased(MouseButton button)
+        {
+            bool result = false;
+
+            switch (button)
+            {
+                case MouseButton.Left:
+
+                    result = mouseState.LeftButton == ButtonState.Released &&
+                        lastMouseState.LeftButton == ButtonState.Pressed;
+
+                    break;
+
+                case MouseButton.Right:
+
+                    result = mouseState.RightButton == ButtonState.Released &&
+                        lastMouseState.RightButton == ButtonState.Pressed;
+
+                    break;
+
+                case MouseButton.Middle:
+
+                    result = mouseState.MiddleButton == ButtonState.Released &&
+                        lastMouseState.MiddleButton == ButtonState.Pressed;
+
+                    break;
+            }
+
+            return result;
+        }
+
+        public static bool IsMouseDown(MouseButton button)
+        {
+            bool result = false;
+
+            switch (button)
+            {
+                case MouseButton.Left:
+
+                    result = mouseState.LeftButton == ButtonState.Pressed;
+
+                    break;
+
+                case MouseButton.Right:
+
+                    result = mouseState.RightButton == ButtonState.Pressed;
+
+                    break;
+
+                case MouseButton.Middle:
+
+                    result = mouseState.MiddleButton == ButtonState.Pressed;
+
+                    break;
+            }
+
+            return result;
+        }
+
+        public static bool IsMouseUp(MouseButton button)
+        {
+            bool result = false;
+
+            switch (button)
+            {
+                case MouseButton.Left:
+
+                    result = mouseState.LeftButton == ButtonState.Released;
+
+                    break;
+
+                case MouseButton.Right:
+
+                    result = mouseState.RightButton == ButtonState.Released;
+
+                    break;
+
+                case MouseButton.Middle:
+
+                    result = mouseState.MiddleButton == ButtonState.Released;
+
+                    break;
+            }
+
+            return result;
         }
     }
 }
