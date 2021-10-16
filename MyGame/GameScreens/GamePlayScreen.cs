a65648dc07cc02c8d87461c8be7ebbf66eb1a206
unknown <dkotov899@gmail.com>
2021-10-16 17:16:14 +0300

    Control picture box

---
+++ MyGame/GameScreens/GamePlayScreen.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+
+using MyGame.GameStates;
+
+namespace MyGame.GameScreens
+{
+    public class GamePlayScreen : BaseGameState
+    {
+        public GamePlayScreen(Game game, GameStateManager manager)
+            : base(game, manager)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
